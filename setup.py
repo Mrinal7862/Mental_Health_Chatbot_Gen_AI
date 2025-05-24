8535d1f249bf179e783f46aa435b51cf91ef86c2
MrinalDev306 <mrinal.devnath435709@gmail.com>
2025-05-25 01:59:03 +0530

    folder_structure_added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI Project", 
+    version="0.0.0",
+    author="Mrinal Devnath",
+    author_email="mrinal.devnath435709@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
